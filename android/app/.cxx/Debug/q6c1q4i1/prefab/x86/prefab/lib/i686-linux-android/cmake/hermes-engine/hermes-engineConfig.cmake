b50fb47ee6671a359f0392c33e3022008d28b294
Gunarathna RP <163238777+RP1999@users.noreply.github.com>
2025-12-19 11:08:00 +0530

    chore: ignore and remove android .cxx build artifacts

--- android/app/.cxx/Debug/q6c1q4i1/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/ranidupromod/.gradle/caches/8.13/transforms/0b4e746a308c8009174132f6c43ff67e/transformed/jetified-hermes-android-0.72.10-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/ranidupromod/.gradle/caches/8.13/transforms/0b4e746a308c8009174132f6c43ff67e/transformed/jetified-hermes-android-0.72.10-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
